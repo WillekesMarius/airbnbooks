89b56850af94d2634219b95dbc14ee08482440bb
Marius Willekes <willekesmarius@gmail.com>
2023-08-21 14:47:39 +0200

    added models

---
+++ app/models/book.rb
@@ -0,0 +1,10 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  has_many :rentals
+
+  validates :title, presence: true
+  validates :year, presence: true
+  validates :author, presence: true
+  validates :overview, presence: true, length: { maximum: 500 }
+  validates :genre, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_115001) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_21_124011) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.integer "year"
+    t.string "author"
+    t.text "overview"
+    t.string "genre"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_rentals_on_book_id"
+    t.index ["user_id"], name: "index_rentals_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_115001) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "users"
+  add_foreign_key "rentals", "books"
+  add_foreign_key "rentals", "users"
 end
