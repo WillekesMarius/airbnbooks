b4be35e6045b492a541e3353b134f04d6e4d6e7b
Marius Willekes <willekesmarius@gmail.com>
2023-08-23 15:19:43 +0200

    some extra additions

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -9,7 +9,7 @@ class RentalsController < ApplicationController
     @rental = Rental.new(rental_params)
     @rental.user = current_user
     @rental.book = @book
-    
+
 #     calculate_days
 
     if @rental.save
@@ -22,13 +22,6 @@ class RentalsController < ApplicationController
 #   def return ## need to be able to return the rented book
 
     # calculate_days
-
-    if @rental.save
-      redirect_to @rental.user, notice: "You have successfully rented this book!"
-    else
-      render 'books/show'
-    end
-  end
 
   private
 
